962139dda6bc41a9aba5c68c6d5687d2da1f844a
BillBaud <billbaudhq@gmail.com>
2026-03-14 18:32:31 +1100

    Improve service worker caching

--- web/sw.js
+++ web/sw.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = "parabreach-v1";
+const CACHE_NAME = "parabreach-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./game.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/sounds/ChopperExplosion.wav",
+  "./assets/sounds/FinalExplosion.wav",
+  "./assets/sounds/MissileExplosion.wav",
+  "./assets/sounds/MissileLaunch.wav",
+  "./assets/sounds/TrooperAlert.wav",
+  "./assets/sounds/TrooperGroundHit.wav",
+  "./assets/sounds/TrooperHit.wav",
+  "./assets/sounds/TurretShoot.wav"
 ];
 
 self.addEventListener("install", event => {
